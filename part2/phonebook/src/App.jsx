689833c6a5f1f72009bd59b8f8aeb6b1de6519bd
AGORKHALY <abhyudaya1063@gmail.com>
2025-01-28 15:37:39 +0545

    getting data from server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import Number from './components/Numbers';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newNumber, setNewNumber] = useState('');
   const [newName, setNewName] = useState('');
   const [filterQuery, setFilterQuery] = useState('');
+
+  const personsData = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      });
+  }
+
+  useEffect(personsData, []);
 
   const addPerson = (event) => {
     event.preventDefault();
